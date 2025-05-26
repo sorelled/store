26e1c5c3b6cfdff97109766ee9c9851208b0fb27
sorelleD <sorelled039@gmail.com>
2025-05-26 10:08:41 +0100

    First commit

---
+++ store/src/main/java/marilane/store/store/StoreApplication.java
@@ -0,0 +1,15 @@
+package marilane.store.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+@EntityScan("model")
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+
+}
---
+++ store/src/main/java/model/TypePoste.java
@@ -0,0 +1,10 @@
+package model;
+
+public enum TypePoste {
+    CADRE,
+    CHEFBUREAU,
+    CHEFSERVICE,
+    DIRECTEUR,
+    DG
+}
+
